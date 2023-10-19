8de95e9674ce748044607c9231f8be1945a3a80b
Marco(양채훈) <yd9506@stclab.com>
2023-10-19 11:33:36 +0900

    :sparkles: 로깅
    사용자로부터 받은 요청 로깅

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import Koa from 'koa';
+import { koaBody } from 'koa-body';
 
 const app = new Koa();
 
+app.use(koaBody());
+const TAB = '  ';
+
+/**
+ * Logger
+ */
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.get('X-Response-Time');
+
   console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  if (ctx.request.body) {
+    console.log(`${TAB}Body ${JSON.stringify(ctx.request.body)}`);
+  }
 });
 
+/**
+ * X-Response-Time
+ */
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
